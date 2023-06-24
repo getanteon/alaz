157806c01e621d49e55440952b5c5998eafb9dfb
kenanfarukcakir <kenanfarukcakir@gmail.com>
2023-06-24 06:07:53 +0000

    prepare db statements once

--- datastore/pg.go
+++ datastore/pg.go
@@ -35,7 +35,8 @@ func connectToPostgresDb(psqlconn string) (*sql.DB, error) {
 }
 
 type Repository struct {
-	db *sql.DB
+	db    *sql.DB
+	stmts map[string]*sql.Stmt
 }
 
 func NewRepository(appConfig config.PostgresConfig) Repository {
@@ -49,22 +50,104 @@ func NewRepository(appConfig config.PostgresConfig) Repository {
 	}
 	fmt.Println("Connected!")
 
-	return Repository{db}
+	r := Repository{db, make(map[string]*sql.Stmt)}
+	r.prepareStatements()
+
+	return r
 }
 
-func (r Repository) Close() {
-	r.db.Close()
+func (r Repository) prepareStatements() {
+	r.stmts["create_pod"] = r.prepareCreatePodStatement()
+	r.stmts["update_pod"] = r.prepareUpdatePodStatement()
+	r.stmts["delete_pod"] = r.prepareDeletePodStatement()
+	r.stmts["create_service"] = r.prepareCreateServiceStatement()
+	r.stmts["update_service"] = r.prepareUpdateServiceStatement()
+	r.stmts["delete_service"] = r.prepareDeleteServiceStatement()
+	r.stmts["create_request"] = r.prepareCreateRequestStatement()
 }
 
-func (r Repository) CreatePod(dto Pod) error {
+func (r Repository) prepareCreatePodStatement() *sql.Stmt {
 	query := fmt.Sprintf("INSERT INTO %s (uid,name,namespace,image,ip) VALUES($1, $2,$3,$4,$5)", podTableName)
 	stmt, err := r.db.Prepare(query)
 	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
+		log.Logger.Error().Err(err).Msg("error preparing create-pod query")
+		return nil
 	}
-	defer stmt.Close()
+	return stmt
+}
 
+func (r Repository) prepareUpdatePodStatement() *sql.Stmt {
+	query := fmt.Sprintf("UPDATE %s SET name = $1, namespace = $2 , image = $3 , ip = $4 WHERE uid = $5", podTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing update-pod query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) prepareDeletePodStatement() *sql.Stmt {
+	query := fmt.Sprintf("DELETE FROM %s WHERE uid = $1", podTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing delete-pod query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) prepareCreateServiceStatement() *sql.Stmt {
+	query := fmt.Sprintf("INSERT INTO %s (uid,name,namespace,type,cluster_ip) VALUES($1, $2,$3,$4,$5)", serviceTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing create-service query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) prepareUpdateServiceStatement() *sql.Stmt {
+	query := fmt.Sprintf("UPDATE %s SET name = $1, namespace = $2 , type = $3,  cluster_ip = $4 WHERE uid = $5", serviceTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing update-service query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) prepareDeleteServiceStatement() *sql.Stmt {
+	query := fmt.Sprintf("DELETE FROM %s WHERE uid = $1", serviceTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing delete-service query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) prepareCreateRequestStatement() *sql.Stmt {
+	query := fmt.Sprintf("INSERT INTO %s (start_time,latency,from_ip,from_type,from_uid,to_ip,to_type,to_uid,protocol,completed,status_code,fail_reason) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)", requestTableName)
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		log.Logger.Error().Err(err).Msg("error preparing create-request query")
+		return nil
+	}
+	return stmt
+}
+
+func (r Repository) Close() {
+
+	for _, stmt := range r.stmts {
+		stmt.Close()
+	}
+
+	r.db.Close()
+
+}
+
+func (r Repository) CreatePod(dto Pod) error {
+	stmt := r.stmts["create_pod"]
 	row := stmt.QueryRow(dto.UID, dto.Name, dto.Namespace, dto.Image, dto.IP)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -74,14 +157,7 @@ func (r Repository) CreatePod(dto Pod) error {
 }
 
 func (r Repository) UpdatePod(dto Pod) error {
-	query := fmt.Sprintf("UPDATE %s SET name = $1, namespace = $2 , image = $3 , ip = $4 WHERE uid = $5", podTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["update_pod"]
 	row := stmt.QueryRow(dto.Name, dto.Namespace, dto.Image, dto.IP, dto.UID)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -91,14 +167,7 @@ func (r Repository) UpdatePod(dto Pod) error {
 }
 
 func (r Repository) DeletePod(dto Pod) error {
-	query := fmt.Sprintf("DELETE FROM %s WHERE uid = $1", podTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["delete_pod"]
 	row := stmt.QueryRow(dto.UID)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -108,14 +177,7 @@ func (r Repository) DeletePod(dto Pod) error {
 }
 
 func (r Repository) CreateService(dto Service) error {
-	query := fmt.Sprintf("INSERT INTO %s (uid,name,namespace,type,cluster_ip) VALUES($1, $2,$3,$4,$5)", serviceTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["create_service"]
 	row := stmt.QueryRow(dto.UID, dto.Name, dto.Namespace, dto.Type, dto.ClusterIP)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -125,14 +187,7 @@ func (r Repository) CreateService(dto Service) error {
 }
 
 func (r Repository) UpdateService(dto Service) error {
-	query := fmt.Sprintf("UPDATE %s SET name = $1, namespace = $2 , type = $3,  cluster_ip = $4 WHERE uid = $5", serviceTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["update_service"]
 	row := stmt.QueryRow(dto.Name, dto.Namespace, dto.Type, dto.ClusterIP, dto.UID)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -142,14 +197,7 @@ func (r Repository) UpdateService(dto Service) error {
 }
 
 func (r Repository) DeleteService(dto Service) error {
-	query := fmt.Sprintf("DELETE FROM %s WHERE uid = $1", serviceTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["delete_service"]
 	row := stmt.QueryRow(dto.UID)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
@@ -159,14 +207,7 @@ func (r Repository) DeleteService(dto Service) error {
 }
 
 func (r Repository) PersistRequest(dto Request) error {
-	query := fmt.Sprintf("INSERT INTO %s (start_time,latency,from_ip,from_type,from_uid,to_ip,to_type,to_uid,protocol,completed,status_code,fail_reason) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)", requestTableName)
-	stmt, err := r.db.Prepare(query)
-	if err != nil {
-		log.Logger.Error().Err(err).Msg("error preparing query")
-		return fmt.Errorf("error preparing query")
-	}
-	defer stmt.Close()
-
+	stmt := r.stmts["create_request"]
 	row := stmt.QueryRow(dto.StartTime.UnixMilli(), dto.Latency, dto.FromIP, dto.FromType, dto.FromUID, dto.ToIP, dto.ToType, dto.ToUID, dto.Protocol, dto.Completed, dto.StatusCode, dto.FailReason)
 	if row.Err() != nil {
 		log.Logger.Error().Err(row.Err()).Msg("Could not execute prepared statement")
